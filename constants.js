d042e9e5e377a71e11931ae04b9c7f64964d3c7d
Leandro Fernandez <leandroofernandezz@gmail.com>
2020-06-06 15:55:53 -0300

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,14 @@
+export const direccion = `
+Dr. Ceraso 1660, Santos Lugares
+`
+
+export const horarios = `
+Horarios de atencion:
+lunes a viernes: 10 - 12am
+`
+
+export const servicios = `
+🎀 Uñas acrílicas
+🎀 Esmaltado semi permanente
+🎀 Depilación definitiva
+`
---
+++ server.js
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv'
+import TelegramBot from 'node-telegram-bot-api'
+import __dirname from './dirname.js'
+import {horarios, servicios, direccion} from './constants.js'
+
+dotenv.config()
+const token = process.env.TELEGRAM_BOT_TOKEN
+
+console.log(__dirname)
+
+const bot = new TelegramBot(token, {polling: true});
+ 
+// Matches "/echo [whatever]"
+bot.onText(/^\/horarios$/, (msg, match) => {
+  const chatId = getChatId(msg)
+ 
+  bot.sendMessage(chatId, horarios);
+})
+
+// Matches "/echo [whatever]"
+bot.onText(/^\/servicios$/, (msg, match) => {
+  const chatId = getChatId(msg)
+ 
+  bot.sendMessage(chatId, servicios);
+})
+
+bot.onText(/^\/foto$/, (msg) => {
+  const chatId = getChatId(msg)
+  const photo = `${__dirname}/fotos/failed.png`;
+  bot.sendPhoto(chatId, photo, {
+    caption: "Salu"
+  });
+})
+
+bot.onText(/^\/ubicacion$/, (msg) => {
+  const chatId = getChatId(msg)
+  bot.sendMessage(chatId, direccion)
+  bot.sendLocation(chatId, -34.602307, -58.541995)
+})
+
+function getChatId(msg) {
+  return msg.chat.id
+}
