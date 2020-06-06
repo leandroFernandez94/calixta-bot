d02d5e9056494b4c40ad64ef0f60606bd3d485fb
leandro <leandroofernandezz@gmail.com>
2020-06-06 16:59:43 -0300

    fix env vars

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ import __dirname from './dirname.js'
 import {horarios, servicios, direccion, metodosDePago} from './constants.js'
 
 dotenv.config()
-const token = process.env.TELEGRAM_BOT_TOKEN
-
-const bot = new TelegramBot(token, {polling: true});
+const {TELEGRAM_BOT_TOKEN: TOKEN, PORT, APP_URL} = process.env
 
 const options = {
   webHook: {
     // Port to which you should bind is assigned to $PORT variable
     // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
-    port: process.env.PORT
+    port: PORT
     // you do NOT need to set up certificates since Heroku provides
     // the SSL certs already (https://<app-name>.herokuapp.com)
     // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
@@ -23,7 +21,7 @@ const options = {
 // Add URL of your app to env variable or enable Dyno Metadata
 // to get this automatically
 // See: https://devcenter.heroku.com/articles/dyno-metadata
-const url = process.env.APP_URL || 'https://calixta-bot.herokuapp.com/';
+const url = APP_URL || 'https://calixta-bot.herokuapp.com/';
 const bot = new TelegramBot(TOKEN, options);
 
 
