b338d2b99d305bc832a608c3ebc202957d83bc74
leandro <leandroofernandezz@gmail.com>
2020-06-06 19:30:01 -0300

    separar envs

--- server.js
+++ server.js
@@ -4,17 +4,22 @@ import __dirname from './dirname.js'
 import applyCommandListeners from './commands/index.js'
 
 dotenv.config()
-const {TELEGRAM_BOT_TOKEN: TOKEN, PORT, APP_URL} = process.env
+const {TELEGRAM_BOT_TOKEN: TOKEN, PORT, APP_URL, NODE_ENV} = process.env
 
-const options = {
-  webHook: {
-    port: PORT
-  }
-};
+let bot = null
 
-const url = APP_URL || 'https://calixta-bot.herokuapp.com/';
-const bot = new TelegramBot(TOKEN, options);
-
-bot.setWebHook(`${url}/bot${TOKEN}`);
+if (NODE_ENV === 'development') {
+  bot = new TelegramBot(TOKEN, {polling: true});
+} else {
+  const options = {
+    webHook: {
+      port: PORT
+    }
+  };
+  
+  const url = APP_URL || 'https://calixta-bot.herokuapp.com/';
+  bot = new TelegramBot(TOKEN, options);
+  bot.setWebHook(`${url}/bot${TOKEN}`);
+}
 
 applyCommandListeners(bot)
