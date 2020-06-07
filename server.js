54e2ded6140780da5de23d89aaa5debbc6ea01cf
leandro <leandroofernandezz@gmail.com>
2020-06-06 22:52:05 -0300

    clean server

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 import dotenv from 'dotenv'
 import Telegraf from 'telegraf'
-import __dirname from './dirname.js'
 import applyCommandListeners from './commands/index.js'
 import applyCallbackOptionsListeners from './optionsCallbacks/index.js'
 
 dotenv.config()
 const {TELEGRAM_BOT_TOKEN: TOKEN, PORT, APP_URL, NODE_ENV} = process.env
 
-let bot = null
-
-bot = new Telegraf(TOKEN);
+const bot = new Telegraf(TOKEN);
 
 if (NODE_ENV !== 'development') {
   bot.telegram.setWebhook(`${APP_URL}/bot${TOKEN}`);
